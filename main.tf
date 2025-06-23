40e5ef4a85211b354b03d11987c5d20d5946bc57
Chany <chany.vargas@upeu.edu.pe>
2025-06-23 00:51:12 -0500

    prueba001

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ module "s3" {
 }
 
 module "lambda" {
-  source           = "https://github.com/Chany1906/aws-lambda.git?ref=main""
+  source           = "git::https://github.com/Chany1906/aws_s3.git?ref=main"
   input_bucket_arn = module.s3.input_bucket_arn
   output_bucket_arn = module.s3.output_bucket_arn
 }
 
 module "ec2" {
-  source           = "https://github.com/Chany1906/aws_ec2.git?ref=main""
+  source           = "git::https://github.com/Chany1906/aws_ec2.git?ref=main"
   output_bucket_arn = module.s3.output_bucket_arn
 }
