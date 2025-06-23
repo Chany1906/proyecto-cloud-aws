771edba32ed310b566b512bfed911c0d1c1ee2f9
Chany <chany.vargas@upeu.edu.pe>
2025-06-23 01:20:29 -0500

    prueba008

--- main.tf
+++ main.tf
@@ -1,18 +1,16 @@
 module "s3" {
-  source = "git::https://github.com/Chany1906/aws_s3.git?ref=main"
-  lambda_arn = module.lambda.lambda_arn
+  source      = "git::https://github.com/Chany1906/aws_s3.git?ref=main"
+  lambda_arn  = module.lambda.lambda_arn
 }
 
 module "lambda" {
-  source           = "git::https://github.com/Chany1906/aws_s3.git?ref=main"
-  input_bucket_arn = module.s3.input_bucket_arn
+  source            = "git::https://github.com/Chany1906/aws-lambda.git?ref=main"
+  input_bucket_arn  = module.s3.input_bucket_arn
   output_bucket_arn = module.s3.output_bucket_arn
-  s3_bucket_name = module.s3.bucket_name
+  s3_bucket_name    = module.s3.bucket_name
 }
 
 module "ec2" {
-  source           = "git::https://github.com/Chany1906/aws_ec2.git?ref=main"
+  source            = "git::https://github.com/Chany1906/aws_ec2.git?ref=main"
   output_bucket_arn = module.s3.output_bucket_arn
 }
-
-#cometario
