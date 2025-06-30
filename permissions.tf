52573de2a9cf942be783ae75529d5d2e5d136015
Chany <chany.vargas@upeu.edu.pe>
2025-06-30 18:34:19 -0500

    limpieza prueba

--- permissions.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_lambda_permission" "allow_s3" {
-  statement_id  = "PermitirEjecucionDesdeS3"
-  action        = "lambda:InvokeFunction"
-  function_name = module.lambda.lambda_function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = module.s3.input_bucket_arn
-}
